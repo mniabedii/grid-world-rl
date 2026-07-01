97d830e925eb1011ff7b0f316c413ee536cca65d
mniabedii <abedii.mni@gmail.com>
2026-07-02 02:40:46 +0330

    implement the dqn

---
+++ network.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+
+class DQN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(2, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 4)
+
+    def forward(self, x):
+
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
